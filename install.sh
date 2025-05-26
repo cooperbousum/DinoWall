283d75aaa30949b2adc0dd631f7766fdce91e86a
Cooper Bousum <cooperbousum@gmail.com>
2025-05-25 23:03:59 -0400

    add install.sh and uninstall.sh

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+current=$(pwd)
+while true; do
+  read -p "Install [l]ocally or [g]lobally [l/g]: " answer
+  case "$answer" in
+  [Ll]*)
+    echo "Installing for current user..."
+    destination="/.local/bin"
+    break
+    ;;
+  [Gg]*)
+    echo "Installing globally..."
+    dsetination="/usr/bin"
+    break
+    ;;
+  *) echo "Please enter l or g." ;;
+  esac
+done
+
+echo -e "\e[33mCreating installation directory\e[0m"
+if [[ -d "$destination/DinoWall" ]]; then
+  sudo rm -rf "$destination/DinoWall"
+fi
+sudo mkdir -p "$destination/DinoWall"
+
+sudo cp -r "$current/bin" "$destination/DinoWall"
+sudo cp -r "$current/commands" "$destination/DinoWall"
+sudo cp -r "$current/lib" "$destination/DinoWall"
+sudo chmod +x "$destination/DinoWall/bin/dino-daemon"
+sudo chmod +x "$destination/DinoWall/bin/dinowall"
+sudo chmod +x "$destination/DinoWall/lib/betweenfiles.sh"
+sudo chmod +x "$destination/DinoWall/lib/cache.sh"
+sudo chmod +x "$destination/DinoWall/lib/count.sh"
+sudo chmod +x "$destination/DinoWall/lib/sunrise.sh"
+sudo chmod +x "$destination/DinoWall/lib/sunset.sh"
+
+if [[ -L /usr/bin/dinowall ]]; then
+  echo -e "\e[31mRemoving old dinowall link...\e[0m"
+  sudo rm /usr/bin/dinowall
+fi
+
+echo -e "\e[33mCreating new dinowall link..\e[0m."
+sudo ln -s "$destination/DinoWall/bin/dinowall" /usr/bin/dinowall
+
+if [[ -L /usr/bin/dino-daemon ]]; then
+  echo -e "\e[31mRemoving old dino-daemon link...\e[0m"
+  sudo rm /usr/bin/dino-daemon
+fi
+
+echo -e "\e[33mCreating new dinowall link...\e[0m"
+sudo ln -s "$destination/DinoWall/bin/dino-daemon" /usr/bin/dino-daemon
+
+echo -e "\e[32mInstalled DinoWall successfully.\e[0m"
+dinowall
---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ -L /usr/bin/dinowall ]]; then
+  echo -e "\e[31mRemoving dinowall link...\e[0m"
+  sudo rm /usr/bin/dinowall
+fi
+
+if [[ -L /usr/bin/dino-daemon ]]; then
+  echo -e "\e[31mRemoving dino-daemon link...\e[0m"
+  sudo rm /usr/bin/dino-daemon
+fi
+
+if [[ -d "$/usr/bin/DinoWall" ]]; then
+  sudo rm -rf "/usr/bin/DinoWall"
+fi
+
+if [[ -d "$HOME/.local/bin/DinoWall" ]]; then
+  sudo rm -rf "$HOME/.local/bin/DinoWall"
+fi
+
+echo -e "\e[32mUninstalled DinoWall successfully"
